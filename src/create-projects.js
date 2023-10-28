83ef7af46bce16d1a17a65d415e80be337a1a802
Ryan Nkansah <ryannkan@gmail.com>
2023-10-27 21:42:18 -0400

    finsihed toDo list need to finish projects

--- src/create-projects.js
+++ src/create-projects.js
@@ -5,10 +5,17 @@ projectsContainer.classList.add("projects-container")
 
 
 const addProject = function () {
+    //creates project variables
     const projectHeader = document.createElement("div")
     const projectForm = document.createElement("form")
     const projectInput = document.createElement("input")
     const projectSubmitBtn = document.createElement("button")
+    const projectHome = document.createElement("button") 
+    //dom manipulation
+    projectHome.textContent = "Home"
+    
+
+
 
 
     projectSubmitBtn.setAttribute("type", "submit")
@@ -16,7 +23,10 @@ const addProject = function () {
     projectsContainer.appendChild(projectForm)
     projectsContainer.appendChild(projectInput)
     projectsContainer.appendChild(projectSubmitBtn)
+    projectsContainer.appendChild(projectHome)
     contentDiv.appendChild(projectsContainer)
+
+
 
 
     
